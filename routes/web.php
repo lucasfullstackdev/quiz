87d049cc029cf0c0a9af22c852b19aa888eb0a6b
lucasfullstackdev <lucas.fullstack.dev@gmail.com>
2020-06-10 15:47:47 -0300

    2020-06-10 v1.3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('questionario/')->name('questionario.')->group( function(){
+    Route::get('/{id_questionario}', 'QuestionarioController@index')->name('index');
 });
